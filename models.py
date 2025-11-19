f12dce6cd772bdba2f47cb99729eaa4c0a72960a
Miih <Miih@gmail.com>
2025-11-18 21:21:03 -0300

    versão 1.0

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Date
 from sqlalchemy.orm import relationship
 from database import Base
 
 class Admin(Base):
     __tablename__= "admin"
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
-    email = Column(String, index=True)
-    senha = Column(String, index=True,unique=True)
+    nome = Column(String, index=True, nullable=False)
+    email = Column(String, index=True, unique=True, nullable=False)
+    senha = Column(String, index=True, nullable=False)
 
    
 class Favorito(Base):
@@ -21,20 +21,20 @@ class Favorito(Base):
 class Editora(Base):
     __tablename__= "editoras"
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
+    nome = Column(String, index=True, nullable=False)
 
 class Autor(Base):
     __tablename__= "autores"
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
+    nome = Column(String, index=True, nullable=False)
 
 class Emprestimo(Base):
     __tablename__= "emprestimos"
     id = Column(Integer, primary_key=True, index=True)
     livro_id = Column(Integer, ForeignKey("livros.id"), nullable=False)
     usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
-    data_retirada = Column(String, index=True)
-    data_devolucao = Column(String, index=True)
+    data_retirada = Column(Date, index=True)
+    data_devolucao = Column(Date, index=True)
 
     alunos = relationship("Livro", back_populates="livros")
     usuarios = relationship("Usuario", back_populates="usuarios") 
@@ -43,14 +43,14 @@ class Emprestimo(Base):
 class Usuario(Base):
     __tablename__= "usuarios"
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String, index=True)
-    email = Column(String, index=True)
-    senha =Column(String, index=True)
+    nome = Column(String, index=True , nullable=False)
+    email = Column(String, index=True, unique=True , nullable=False)
+    senha =Column(String, index=True, nullable=False)
 
 class Livro(Base):
     __tablename__= "livros"
     id = Column(Integer, primary_key=True, index=True)
-    titulo = Column(String, index=True)
+    titulo = Column(String, index=True, nullable=False)
     autor_id = Column(Integer, ForeignKey("autores.id"), nullable=False)
     editora_id = Column(Integer, ForeignKey("editoras.id"), nullable=False)
 
